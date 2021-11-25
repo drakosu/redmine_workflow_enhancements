drakosu/redmine_workflow_enhancements

commit a480756cc3a65307e74ca846bd8df3ef61301715

Filters workspaces

lib/workflow_enhancements/graph.rb
@@ -1,6 +1,6 @@
 module WorkflowEnhancements::Graph
 
-  def self.load_data(roles, trackers, issue=nil, project_roles=nil)
+  def self.load_data(roles, trackers, issue=nil, project_roles=nil, workspace_id=nil)
     tracker = nil
     if trackers.is_a?(Array)
       tracker = trackers.length == 1 ? trackers.first : nil
@@ -23,7 +23,7 @@ module WorkflowEnhancements::Graph
 
     new_issue_status_map = {}
     edges_map = {}
-    WorkflowTransition.where(:tracker_id => tracker).each do |t|
+    WorkflowTransition.where(:tracker_id => tracker, :workspace_id => workspace_id).each do |t|
       next unless project_roles.nil? || project_roles.include?(t.role_id)
       if t.old_status_id != 0
         key = t.old_status_id.to_s + '-' + t.new_status_id.to_s

app/views/workflow_enhancements/_workflow_graph.html.erb
@@ -55,7 +55,7 @@
 
   <script type="text/javascript">
   $(function() {
-    var json = <%= WorkflowEnhancements::Graph.load_data(roles, trackers, issue, project_role_ids).to_json.html_safe %>;
+    var json = <%= WorkflowEnhancements::Graph.load_data(roles, trackers, issue, project_role_ids, @project && @project.workspace_id ? @project.workspace_id : params[:workspace_id]).to_json.html_safe %>;
 
     var renderer = new dagreD3.Renderer();
     var oldDrawNodes = renderer.drawNodes();

commit 20b8da7dc8ef959d4d773cb90c2f2d594887305b

Fix: 500 error caused by workspace_id from a480756cc3a65307e74ca846bd8df3ef61301715

lib/workflow_enhancements/graph.rb
@@ -23,7 +23,9 @@ module WorkflowEnhancements::Graph
 
     new_issue_status_map = {}
     edges_map = {}
-    WorkflowTransition.where(:tracker_id => tracker, :workspace_id => workspace_id).each do |t|
+    option = {:tracker_id => tracker}
+    option[:workspace_id] = workspace_id if workspace_id
+    WorkflowTransition.where(option).each do |t|
       next unless project_roles.nil? || project_roles.include?(t.role_id)
       if t.old_status_id != 0
         key = t.old_status_id.to_s + '-' + t.new_status_id.to_s